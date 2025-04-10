19d092fc76a84e069d4265d8111b78aabbd82b3c
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-04-10 23:36:29 +0530

    Funky Countdown With a Plot Twist

---
+++ Funky Countdown With a Plot Twist .py
@@ -0,0 +1,15 @@
+# Signing in
+
+import time
+import random
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(0.5)
+
+if random.random() > 0.5:
+    print("Boom! 💥 Just kidding. You passed.")
+else:
+    print("Psych! Nothing happened... or did it? 👀")
+
+# Signing off 
